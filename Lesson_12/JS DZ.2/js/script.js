ec1770c333d0d67f88de03d8b6f903245b64cccd
Filatov Dmytro <dmitrovfilatov@gmail.com>
2024-06-20 22:56:44 +0200

    dz
    dz

--- Lesson_12/JS DZ.2/js/script.js
+++ Lesson_12/JS DZ.2/js/script.js
@@ -1,8 +1,13 @@
 
-const ColorBlock = document.querySelector(".block");
-const changeColorAction = document.querySelector("#butColor");
-console.log(changeColorAction);
+const ButtonBlock = document.querySelector("#buttonsDiv");
+const Resut = document.querySelector("#result");
 
-changeColorAction.addEventListener('click', function(event) {
-  ColorBlock.classList.toggle('changeColor');
+ButtonBlock.addEventListener('click', (event) => {
+  const test = event.target.textContent;
+  alert(`Ви клікнули по кнопці: ${test}`);
 });
+
+// ButtonBlock.addEventListener('click', (event) => {
+//   const test = event.target.textContent;
+//   Resut.textContent = test;
+// });
