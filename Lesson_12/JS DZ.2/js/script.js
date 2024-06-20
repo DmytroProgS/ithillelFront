350b7f7d9a18d4f64d78970f04a6e4c243325b6f
Filatov Dmytro <dmitrovfilatov@gmail.com>
2024-06-20 22:57:52 +0200

    dz
    dz

--- Lesson_12/JS DZ.2/js/script.js
+++ Lesson_12/JS DZ.2/js/script.js
@@ -6,8 +6,3 @@ ButtonBlock.addEventListener('click', (event) => {
   const test = event.target.textContent;
   alert(`Ви клікнули по кнопці: ${test}`);
 });
-
-// ButtonBlock.addEventListener('click', (event) => {
-//   const test = event.target.textContent;
-//   Resut.textContent = test;
-// });
