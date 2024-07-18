6677c4dca642c6a432f7e034926c7ce098ba1350
Filatov Dmytro <dmitrovfilatov@gmail.com>
2024-07-18 23:50:49 +0200

    dz
    dz

--- Lesson_17/17.3/js/main.js
+++ Lesson_17/17.3/js/main.js
@@ -8,18 +8,20 @@ class BankAccount {
        return this.stateOfAccount;
     }
     deposit(money) {
+        this.stateOfAccount += money;
         return this.stateOfAccount;
     }
 
     withdraw(money) {
-
+        this.stateOfAccount -= money;
+        return this.stateOfAccount;
     }
 
 }
     
-    const account1 = new BankAccount(1000);
-    console.log(account1.getBalance()); // 1000
-    account1.deposit(500);
-    console.log(account1.getBalance()); // 1500
-    account1.withdraw(200);
-    console.log(account1.getBalance()); // 1300
+const account1 = new BankAccount(1000);
+console.log(account1.getBalance());
+account1.deposit(500);
+console.log(account1.getBalance());
+account1.withdraw(200);
+console.log(account1.getBalance());
