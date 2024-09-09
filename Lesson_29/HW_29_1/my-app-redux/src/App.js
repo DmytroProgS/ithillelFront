6ea406a1d1bcaecede2ef5088dda63918dedd388
DmytroProgS <dmytrogerua@gmail.com>
2024-09-09 14:00:02 +0200

    dz
    dz

---
+++ Lesson_29/HW_29_1/my-app-redux/src/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import Counter from '../src/components/Counter/Counter.js';
+import '../src/styles/styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
